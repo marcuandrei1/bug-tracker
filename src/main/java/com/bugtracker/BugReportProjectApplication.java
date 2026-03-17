1d6999f9b37a734681886dd0d85b4084d84aa48f
Andrei Marcu <@marcuandrei1>
2026-03-17 20:55:55 +0200

    adaugare conexiune baza de date

---
+++ src/main/java/com/bugtracker/BugReportProjectApplication.java
@@ -0,0 +1,15 @@
+package com.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugReportProjectApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Heloooooooooooooooo");
+        SpringApplication.run(BugReportProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bugtracker/TestEntity.java
@@ -0,0 +1,16 @@
+package com.bugtracker;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class TestEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
